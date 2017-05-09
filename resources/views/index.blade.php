5f9297c057a4c89484c80706f8421de76776f9e6
castion2293 <castion2293@yahoo.com.tw>
2017-05-09 15:12:09 -0600

    initial the index blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -27,12 +27,37 @@
             background-position: center;
             background-repeat: no-repeat;
             background-size: cover;
-            padding: 16em 0em 14em;
             background-image: url('https://s3.amazonaws.com/robotechcloud/Industrial-Robots-Teaser.jpg');
         }
     </style>
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <!-- Latest compiled and minified JavaScript -->
+
+    <script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
 </head>
 <body>
-    <div class="bgimg" style="height: 100%;"></div>
+    <div class="bgimg" style="height:100%;padding-top:15em;">
+        <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-8" style="background-color: rgba(241, 241, 241, .5);">
+                <form class="">
+                    <h1>Robotech Cloud</h1>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <button type="submit" class="btn btn-primary form-control">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
